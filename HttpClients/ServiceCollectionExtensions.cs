5a2f5b59d71290f77370584fd5fe127f1f75ac16
Oleksii <zakharuk21@gmail.com>
2025-05-12 20:24:50 +0300

    Add files via upload
    HttpClientReg

---
+++ HttpClients/ServiceCollectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HttpClients;
+
+public static class ServiceCollectionExtensions
+{
+    private const string DefaultHttpClientName = "Library.HttpClient";
+
+    // Register department http client
+    public static void RegisterDepartmentsHttpClient(this IServiceCollection services, SystemHttpClientData systemHttpClientData)
+    {
+        services.RegisterHttpClient<IDepartmentsHttpClient, DepartmentsHttpClient>(
+            systemHttpClientData,
+            systemHttpClientData.HttpClientName ?? DefaultHttpClientName,
+            httpClient => new DepartmentsHttpClient(httpClient));
+    }
+
+    // Register Faculties http client
+    public static void RegisterFacultiesHttpClient(this IServiceCollection services, SystemHttpClientData systemHttpClientData)
+    {
+        services.RegisterHttpClient<IFacultiesHttpClient, FacultiesHttpClient>(
+            systemHttpClientData,
+            systemHttpClientData.HttpClientName ?? DefaultHttpClientName,
+            httpClient => new FacultiesHttpClient(httpClient));
+    }
+
+
+    // Register Groups http client
+    public static void RegisterGroupsHttpClient(this IServiceCollection services, SystemHttpClientData systemHttpClientData)
+    {
+        services.RegisterHttpClient<IGroupsHttpClient, GroupsHttpClient>(
+            systemHttpClientData,
+            systemHttpClientData.HttpClientName ?? DefaultHttpClientName,
+            httpClient => new GroupsHttpClient(httpClient));
+    }
+
+    // Register Students http client
+    public static void RegisterStudentsHttpClient(this IServiceCollection services, SystemHttpClientData systemHttpClientData)
+    {
+        services.RegisterHttpClient<IStudentsHttpClient, StudentsHttpClient>(
+            systemHttpClientData,
+            systemHttpClientData.HttpClientName ?? DefaultHttpClientName,
+            httpClient => new StudentsHttpClient(httpClient));
+    }
+   
+
+    public static void RegisterHttpClient<TInterface, TImplementation>(
+        this IServiceCollection services,
+        SystemHttpClientData systemHttpClientData,
+        string name,
+        Func<HttpClient, TImplementation> func)
+        where TInterface : class
+        where TImplementation : TInterface
+    {
+        HttpClientRegistrator.RegisterHttpClient<TInterface, TImplementation>(services, systemHttpClientData, name, func);
+    }
+}
